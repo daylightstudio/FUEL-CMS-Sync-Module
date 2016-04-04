749c0dd5e427d9a085d4a7ce9a5963f9b8ef17c1
David McReynolds <dave@bydaylight.com>
2016-04-04 12:28:57 -0700

    Update initialize func to be compatible with CI 3

--- libraries/Fuel_sync.php
+++ libraries/Fuel_sync.php
@@ -77,7 +77,7 @@ class Fuel_sync extends Fuel_advanced_module {
 	 * @param	array	config preferences
 	 * @return	void
 	 */	
-	function initialize($params)
+	function initialize($params = array())
 	{
 		parent::initialize($params);
 		$this->set_params($this->_config);
